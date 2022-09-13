2838f51a6463188bad3d8f21f9ae1cae3937c14d
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-13 14:04:04 +0300

    Task 4 completed

---
+++ 1.Syntax, Functions and Statements/CircleArea.js
@@ -0,0 +1,16 @@
+function CalculateArea(inputValue) 
+{
+    let result = undefined;
+    if(typeof(inputValue) !== 'number')
+    {
+        console.log(`We can not calculate the circle area, because we receive a ${typeof(inputValue)}.`);
+    } 
+    else
+    {
+         result = Math.PI * Math.pow(inputValue,2);
+         console.log(result.toFixed(2));
+    }
+}
+
+
+CalculateArea('asd')
