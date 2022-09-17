9dc3dab80d65d81dc868618fcb20734aca187fba
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-17 15:59:24 +0300

    Task 10 completed

--- 2.Arrays and Nested Arrays/BiggestElement.js
+++ 2.Arrays and Nested Arrays/BiggestElement.js
@@ -8,7 +8,7 @@ function ReturnBiggestElementInNestedArray(inputArray)
         biggestValue = x;
     })
   }
-  console.log(biggestValue);
+
   return biggestValue;
 }
 
---
+++ 2.Arrays and Nested Arrays/DiagonalSums.js
@@ -0,0 +1,29 @@
+function CalculateDiagonalSums(inputArray){
+    let firstIndex = 0;
+    let lastIndex = inputArray.length - 1;
+    let mainSum = 0;
+    let secondarySum = 0;
+
+    for(row = 0; row < inputArray.length; row ++){
+        let currentRow = inputArray[row];
+       for(col = 0; col < currentRow.length; col++){
+        mainSum += currentRow[firstIndex];
+        secondarySum += currentRow[lastIndex];
+        firstIndex++;
+        lastIndex--;
+        break;
+       }
+    }
+
+    console.log(mainSum);
+    console.log(secondarySum);
+}
+
+CalculateDiagonalSums([[20, 40],
+    [10, 60]]
+   );
+
+CalculateDiagonalSums([[3, 5, 17],
+    [-1, 7, 14],
+    [1, -8, 89]]
+   )
