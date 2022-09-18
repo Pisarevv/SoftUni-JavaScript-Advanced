2b875f4137f58875ad316ccc19dbf18b7ddcd3fd
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-18 21:55:41 +0300

    Update

--- 1.Syntax, Functions and Statements/DayOfWeek.js
+++ 1.Syntax, Functions and Statements/DayOfWeek.js
@@ -4,7 +4,7 @@ function ReturnDayOfWeek(input) {
         case'Monday': console.log(1); break;
         case'Tuesday': console.log(2); break;
         case'Wednesday': console.log(3); break;
-        case'Thrusday': console.log(4); break;
+        case'Thursday': console.log(4); break;
         case'Friday': console.log(5); break;
         case'Saturday': console.log(6); break;
         case'Sunday': console.log(7); break;
---
+++ 1.Syntax, Functions and Statements/DaysInAMonth.js
@@ -0,0 +1,9 @@
+function ReturnDaysOfAMonth(month,year)
+{
+   let rawDate = New da( year,month);
+   console.log(rawDate)
+}
+
+
+
+ReturnDaysOfAMonth(1, 2012);
