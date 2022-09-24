0d22a6be88b8e685f2b490cffb9c104471e1018a
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-24 20:07:21 +0300

    Task 3 completed

--- 4.DOM Introduction/03.Sum Numbers/SumNumbers.js
+++ 4.DOM Introduction/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,9 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    debugger;
+    let firstNumber = document.getElementById("num1");
+    let secondNumber = document.getElementById("num2");
+    let resultNode = document.getElementById("sum");
+
+    let result = Number(firstNumber.value) + Number(secondNumber.value);
+    resultNode.value = result;
 }
