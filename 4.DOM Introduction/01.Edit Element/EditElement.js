840f5f4eb388980f91c60d065263e93957d52851
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-24 19:32:58 +0300

    Task 1 completed

--- 4.DOM Introduction/01.Edit Element/EditElement.js
+++ 4.DOM Introduction/01.Edit Element/EditElement.js
@@ -1,3 +1,15 @@
-function editElement() {
-    // TODO
-}
+function editElement(ref,match,replacer) {
+    debugger;
+    let pattern = /\%(.*?)\%/;
+    let text = ref.textContent;
+    while(text.includes(match)){
+        text = text.replace(match,replacer);
+    }
+
+    ref.textContent = text;
+    }
+    
+    
+ 
+
+
