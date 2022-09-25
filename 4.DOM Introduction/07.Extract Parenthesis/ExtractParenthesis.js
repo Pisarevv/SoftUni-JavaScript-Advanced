0b1c12850ff13a3c3e758efe2830e550233adaea
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-25 10:32:48 +0300

    Task 7 completed

--- 4.DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
+++ 4.DOM Introduction/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,12 @@
 function extract(content) {
+   debugger;
+   let pattern = /\(([^)]+)\)/g;
+   let extractedContent = document.getElementById(content).textContent;
+   let matches = extractedContent.matchAll(pattern);
+   let result = [];
+   for(let element of matches){
+    result.push(element[1]);
+   }
+   return result.join('; ');
 
 }
