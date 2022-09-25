7c1cae41a5b0ff3d0ae3a24e5704a78356129b77
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-25 20:46:18 +0300

    Task 2 completed with a trick based on judge requirement

--- 4.DOM Introduction - Exercise/03. Accordion/accordion.js
+++ 4.DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,18 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonInformation = document.getElementsByClassName("button")[0];
+    let hiddenText = document.getElementById("extra");
+    if(hiddenText.style.display ==""){
+        hiddenText.style.display = "none";
+    }
+    if(hiddenText.style.display === "none"){
+        hiddenText.style.display = "block";
+        buttonInformation.textContent = "Less";
+    }
+    else if(hiddenText.style.display === "block"){
+        hiddenText.style.display = "none";
+        buttonInformation.textContent = "More";
+    }
+    
+
+
 }
