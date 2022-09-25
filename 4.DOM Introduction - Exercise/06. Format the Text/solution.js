bb5bfb6bc5a918f1661fbd161db394bb7ba05fd8
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-25 22:30:30 +0300

    Task 6 completed

--- 4.DOM Introduction - Exercise/06. Format the Text/solution.js
+++ 4.DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+  let extractedText = document.getElementById("input").value;
+  let splitData = extractedText.split(".").filter(x => x.length > 0);
+  let outputNode = document.getElementById("output");
+  let result = '';
+  for(let i = 0; i<splitData.length ; i+=3){
+    let buffer = '<p>';
+    for(let x = 0 ; x < 3; x++){
+      if(splitData[i+x]){
+        buffer += splitData[i+x] + '.';
+      }
+    }
+    buffer +='</p>';
+    result += buffer;
+  }
+  outputNode.innerHTML = result;
 }
