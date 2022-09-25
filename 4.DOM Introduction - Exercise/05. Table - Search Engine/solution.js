c94bd9583a981db8553ee5ef3cdaa52afaef762e
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-25 21:51:07 +0300

    Task 5 completed

--- 4.DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ 4.DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,15 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+      let extractedNodes = document.querySelectorAll(".container tbody tr");
+      let textToSearch = document.getElementById("searchField").value;
+      for(let node of extractedNodes){
+            node.classList.remove('select');  
+      }
+      for(let node of extractedNodes){
+         if(node.innerHTML.includes(textToSearch)){
+            node.classList.add('select');
+         }
+      }
    }
 }
