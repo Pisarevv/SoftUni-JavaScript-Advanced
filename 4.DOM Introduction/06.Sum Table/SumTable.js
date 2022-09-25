8f6d683c3a0377969921db1dc298062916a34c08
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-25 09:28:24 +0300

    Task 6 completed

--- 4.DOM Introduction/06.Sum Table/SumTable.js
+++ 4.DOM Introduction/06.Sum Table/SumTable.js
@@ -1,3 +1,12 @@
 function sumTable() {
+    debugger;
+   let selected = document.querySelectorAll("table tbody tr");
+   let result = 0;
+   for (let i = 1; i< selected.length -1; i++){
+       let extracted = selected[i].children;
+       result+= Number(extracted[1].textContent);
+   }
 
+   let targetNode = document.getElementById("sum");
+   targetNode.textContent = result;
 }
