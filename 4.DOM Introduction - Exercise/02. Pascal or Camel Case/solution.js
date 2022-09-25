4dedaa77fffad9f5c403bcfad01c65a6e182e37d
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-25 20:22:49 +0300

    Task 2 completed

--- 4.DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ 4.DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-  //TODO...
+  let inputText = document.getElementById("text").value;
+  let typeCase = document.getElementById("naming-convention").value;
+  let outputNode = document.getElementById("result");
+  let result = '';
+
+  switch(typeCase){
+    case"Camel Case":{
+      let stringToModify = inputText.split(" ");
+      for(let i = 0; i < stringToModify.length; i++){
+        let currPart = stringToModify[i].toLowerCase();
+        if(i == 0){
+          result += currPart;
+          continue;
+        }
+        result += currPart[0].toUpperCase() + currPart.substring(1);
+      }
+      break;
+    }
+    case"Pascal Case":{
+      let stringToModify = inputText.split(" ");
+      for(let i = 0; i < stringToModify.length; i++){
+        let currPart = stringToModify[i].toLowerCase();
+        result += currPart[0].toUpperCase() + currPart.substring(1);
+      }
+      break;
+    }
+    default:
+      result = "Error!";
+  }
+   outputNode.textContent = result;
+
 }
