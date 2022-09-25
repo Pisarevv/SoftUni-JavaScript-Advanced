857687e8bbdfa6f5b97dd09db82f46b9fc6aaf63
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-25 09:16:13 +0300

    Task 5 completed

--- 4.DOM Introduction/05.Colorize Table/ColorizeTable.js
+++ 4.DOM Introduction/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,10 @@
 function colorize() {
-    // TODO
+    debugger;
+    let matched = document.querySelectorAll("table tr");
+    for(let i = 0; i < matched.length; i++){
+        if(i % 2 != 0){
+            matched[i].style.backgroundColor = "Teal";
+        }
+    }
+    
 }
