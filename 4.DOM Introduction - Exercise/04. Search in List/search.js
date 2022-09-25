6ba2529d3e5d0a4b045365adad42db3a959a3b26
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-25 21:26:54 +0300

    Task 4 completed

--- 4.DOM Introduction - Exercise/04. Search in List/search.js
+++ 4.DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+   let selectedItems = document.querySelectorAll("#towns li");
+   let elementToContain = document.getElementById("searchText").value;
+   let matchesNode = document.getElementById("result");
+   let matches = 0;
+   for(let node of selectedItems){
+     if(node.innerText.includes(elementToContain)){
+      node.style.fontWeight = "bold";
+      node.style.textDecoration = "underline";
+      matches ++;
+     }
+     else{
+      node.style.fontWeight = "normal";
+      node.style.textDecoration= "none";  
+     }
+   }
+   matchesNode.innerText = `${matches} matches found`;
 }
