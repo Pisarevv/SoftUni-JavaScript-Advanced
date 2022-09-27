140be04bc8505cb36f097426540249a4336b7900
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-28 00:16:01 +0300

    task 9 completed on 50%

--- 4.DOM Introduction - Exercise/09. Number Convertor/solution.js
+++ 4.DOM Introduction - Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,22 @@
 function solve() {
 
-    //TODO...
+    let button = document.querySelectorAll("button")[0];
+    button.addEventListener("click", convert); 
+    let inputValue = document.getElementById("input").value;
+
+    let outputType = document.querySelectorAll("select#selectMenuTo")[0];
+    let binarySelect = document.createElement("option");
+
+    binarySelect.innerText = 'Binary';
+    binarySelect.value = 'binary';
+    outputType.appendChild(binarySelect);
+    let hexadecimalSelect = document.createElement("option");
+    hexadecimalSelect.innerText = 'Hexadecimal';
+    hexadecimalSelect.value = 'hexadecimal';
+    outputType.appendChild(hexadecimalSelect);
+
+
+    function convert(){
+
+    }
 }
