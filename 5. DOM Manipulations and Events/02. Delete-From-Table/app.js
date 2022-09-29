773004e6c0e3406379628691cb4086c55bade069
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-29 21:55:06 +0300

    Task 2 completed

--- 5. DOM Manipulations and Events/02. Delete-From-Table/app.js
+++ 5. DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,14 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let inputValueElement = document.getElementsByName("email")[0];
+    let inputEmail = inputValueElement.value;
+    let deleted = false;
+    let tableRows = document.querySelectorAll("tbody tr");
+    for(let row of tableRows){
+        if(row.children[1].textContent == inputEmail){
+            row.remove();
+            deleted = true;
+        }
+    }
+    let outputElement = document.getElementById("result");
+    deleted == true ? outputElement.textContent = "Deleted." : outputElement.textContent = "Not found.";
 }
