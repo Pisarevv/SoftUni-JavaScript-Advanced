47c7dc904c2deb4ca49b64554f573108c2c201bc
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-29 23:11:37 +0300

    task 5 completed on 50%

--- 5. DOM Manipulations and Events/05. Highlight-Active/app.js
+++ 5. DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,3 +1,10 @@
 function focused() {
-    console.log('TODO:...');
+    let takenElement = document.querySelector("h1");
+    takenElement.addEventListener('mousemove', focus);
+    takenElement.addEventListener('mouseout', unfocus);
+
+    function focus(event){
+        let currentElement = event.target;
+        currentElement.classList.add("focused");
+    }
 }
