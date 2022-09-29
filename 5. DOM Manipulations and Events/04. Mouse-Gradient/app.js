f8527908a1e323b16fed82afe50b935d0f4b161d
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-29 22:38:59 +0300

    task 5 completed

--- 5. DOM Manipulations and Events/04. Mouse-Gradient/app.js
+++ 5. DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,23 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let element = document.getElementById("gradient");
+    element.addEventListener('mousemove', calculateGradient);
+    element.addEventListener('mouseout', mouseOut);
+
+
+
+
+    function calculateGradient(e) {
+        let power = e.offsetX / (e.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+        document.getElementById('result').textContent = power + "%";
+
+
+    }
+
+    function mouseOut(){
+        document.getElementById('result').textContent = "";
+
+    }
+        
+    
 }
