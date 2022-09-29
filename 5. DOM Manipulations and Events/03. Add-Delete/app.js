4ffaa5f47c528a24e77f350ec08a0fb6aadb12a0
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-29 22:29:32 +0300

    Task 3 completed

--- 5. DOM Manipulations and Events/03. Add-Delete/app.js
+++ 5. DOM Manipulations and Events/03. Add-Delete/app.js
@@ -1,3 +1,20 @@
 function addItem() {
-    //TODO...
+    let inputValue = document.getElementById("newItemText").value;
+    let output = document.getElementById("items");
+    let liElement = document.createElement('li');
+    liElement.textContent = inputValue;
+    let remove = document.createElement('a');
+    let linkText = document.createTextNode("[Delete]");
+    remove.appendChild(linkText);
+    remove.href = "#";
+    remove.addEventListener('click', deleteItem);
+    liElement.appendChild(remove);
+    document.getElementById("newItemText").value = '';
+    output.appendChild(liElement);
+
+    function deleteItem(){
+        liElement.remove();
+    }
+    
+
 }
