ea54f99e24d6a30424596b9a3f168fa5c4f43c36
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-30 11:09:55 +0300

    Task 6 completed

--- 5. DOM Manipulations and Events/06. Dynamic-Validation/app.js
+++ 5. DOM Manipulations and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,20 @@
 function validate() {
-    console.log('TODO:...');
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    let inputEmail = document.getElementById("email");
+
+    inputEmail.addEventListener('change', validateEmail);
+
+
+
+
+    function validateEmail(){
+        let inputValue = inputEmail.value;
+        if(!inputValue.match(emailRegex)){
+            inputEmail.classList.add("error");
+        }
+        else{
+            inputEmail.classList.remove("error");
+        }
+    }
 }
