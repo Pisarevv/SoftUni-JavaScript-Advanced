74bd25a7243f73fa455b6a97cec9213ee89c3d40
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-09-30 16:37:21 +0300

    Task 5 in process of finishing

--- 5. DOM Manipulations and Events/07. Shopping-Cart/solution.js
+++ 5. DOM Manipulations and Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,32 @@
 function solve() {
-   //TODO...
+   let addButtons = document.getElementsByClassName("add-product");
+   let boughtProducts = [];
+   for(let button of addButtons){
+      button.addEventListener('click', collectItems);
+   }
+
+
+
+
+   function collectItems(e){
+      let currentDiv = e.target.parentElement.parentElement;
+      let productName = currentDiv.children[1].children[0].textContent;
+      let productPrice = Number(currentDiv.children[3].textContent);
+      
+      if(!boughtProducts.some(x => x.name == productName)){
+         let productObject = {
+            name : productName,
+            price : Number(productPrice)
+         }
+         boughtProducts.push(productObject);
+
+      }
+      else{
+         let currentObject = boughtProducts.filter(x => x.name == productName);
+         currentObject.price = currentObject.price + productPrice;
+         console.log(currentObject.price)
+         
+      }
+      
+   }
 }
