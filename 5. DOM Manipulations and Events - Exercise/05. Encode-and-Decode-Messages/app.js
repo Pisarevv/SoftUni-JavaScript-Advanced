6af4fccd807b860b4109dd7aebd0a969579dc75b
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-10-01 17:04:02 +0300

    Task 5 completed

--- 5. DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ 5. DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,38 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    //buttons
+    let buttons = document.querySelectorAll("button");
+    let encodeButton = buttons[0];
+    let decodeButton = buttons[1];
+    //massages
+    let textAreas = document.querySelectorAll("textarea");
+    inputArea = textAreas[0];
+    outputArea = textAreas[1];
+
+    encodeButton.addEventListener('click', encodeAndSend);
+    decodeButton.addEventListener('click', decodeAndRead);
+
+    function encodeAndSend () {
+        let inputText = inputArea.value;
+        let encodedString = "";
+        for(let i = 0; i < inputText.length; i++){
+            let newChCode = Number(inputText.charCodeAt([i])) + 1;
+            encodedString += (String.fromCodePoint(newChCode));
+
+        }
+        outputArea.value = encodedString;
+        inputArea.value = "";    
+
+    }
+
+    function decodeAndRead () {
+        let inputText = outputArea.value;
+        let decodedString = "";
+        for(let i = 0; i < inputText.length; i++){
+            let newChCode = Number(inputText.charCodeAt([i])) - 1;
+            decodedString += (String.fromCodePoint(newChCode));
+
+        }
+        outputArea.value = decodedString;
+
+    }
 }
