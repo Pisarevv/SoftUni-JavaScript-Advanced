4fa40393c8dd5f1e22c863f668cfdde89b5d8186
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-10-01 12:48:40 +0300

    Task 1 complete

--- 5. DOM Manipulations and Events - Exercise/01. Sections/script.js
+++ 5. DOM Manipulations and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+   let targetOutputElement = document.getElementById("content");
+   for(let word of words){
+      let newDiv = document.createElement("div");
+      newDiv.addEventListener('click', showChildren);
+      let newParagraph = document.createElement("p");
+      newParagraph.textContent = word;
+      newParagraph.style.display = "none";
+      newDiv.appendChild(newParagraph);
+      targetOutputElement.appendChild(newDiv);
+   }
+
+
+
+   function showChildren(e){
+      e.target.children[0].style.display = "";
+   }
 }
