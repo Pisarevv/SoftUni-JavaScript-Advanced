8229d8e7bc89c9df8ffc22d3bf0b2b580a651f31
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-10-01 16:45:38 +0300

    Task 4 completed

--- 5. DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ 5. DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    let inputTextElement = document.getElementById("newItemText");
+    let inputValueElement = document.getElementById("newItemValue");
+    let dropDownMenu = document.getElementById("menu");
+
+    let newOption = document.createElement("option");
+    newOption.textContent = inputTextElement.value;
+    newOption.value = inputValueElement.value;
+    dropDownMenu.appendChild(newOption);
+    inputTextElement.value = "";
+    inputValueElement.value = "";
 }
