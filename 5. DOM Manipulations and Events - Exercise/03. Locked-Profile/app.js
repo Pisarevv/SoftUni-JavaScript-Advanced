624c28589b8611d2340319f19fc10c644d1a0e0b
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-10-01 15:33:18 +0300

    Task 3 completed but judge not accepting it

--- 5. DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
+++ 5. DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    let userProfiles = document.getElementsByClassName("profile");
+    let clickButtons = document.querySelectorAll(".profile button");
+    for(let button of clickButtons){
+        button.addEventListener("click", formatOutput);
+    }
+
+
+    function formatOutput(e){
+        let profile = e.target.parentNode;
+        let isHidden = profile.children[2].checked ? true : false;
+
+        if(!isHidden){
+            profile.children[9].style.display = "block";
+            e.target.textContent = "Hide";
+        }
+        else{
+            profile.children[9].style.display = "none";
+            e.target.textContent = "Show more";
+        }
+
+    }
 }
