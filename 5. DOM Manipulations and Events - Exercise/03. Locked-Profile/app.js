acdc2a6f28487629cd3646cde4a285bf866ca239
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-10-01 16:10:46 +0300

    Task 3 completed

--- 5. DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
+++ 5. DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
@@ -9,16 +9,31 @@ function lockedProfile() {
 
     function formatOutput(e){
         let profile = e.target.parentNode;
-        let isHidden = profile.children[2].checked ? true : false;
+        //let isHidden = profile.querySelectorAll('input[type="radio"]')[0].checked ? true : false;
+       /* let isHidden;
+        if (profile.children[2].checked){
+            isHidden = isHidden;
+        }
+        else if (profile.children[4].checked){e
+            isHidden = !isHidden;
+        }      */ 
+        
 
-        if(!isHidden){
-            profile.children[9].style.display = "block";
-            e.target.textContent = "Hide";
+        if(profile.querySelectorAll('input[type="radio"]')[1].checked){
+
+            console.log(profile.getElementsByTagName("div")[0].style.display);
+
+            if(profile.getElementsByTagName("div")[0].style.display == ""){
+            profile.getElementsByTagName("div")[0].style.display = "block";
+            e.target.textContent = "Hide it";
+            }
+            else if(profile.getElementsByTagName("div")[0].style.display == "block")
+            {
+                profile.getElementsByTagName("div")[0].style.display = "";
+                e.target.textContent = "Show more";
+            }
         }
-        else{
-            profile.children[9].style.display = "none";
-            e.target.textContent = "Show more";
-        }
+        
 
     }
 }
