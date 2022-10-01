d9520dad5df7057e584ea4a16151c647e93595ce
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-10-01 12:32:26 +0300

    Task 7 completed on 97%

--- 5. DOM Manipulations and Events/07. Shopping-Cart/solution.js
+++ 5. DOM Manipulations and Events/07. Shopping-Cart/solution.js
@@ -1,12 +1,14 @@
 function solve() {
-   let addButtons = document.getElementsByClassName("add-product");
+   let addButtons = Array.from(document.getElementsByClassName("add-product"));
+   let checkOutButton = document.getElementsByClassName("checkout")[0];
+   let targetOutput = document.getElementsByTagName("textarea")[0];
    let boughtProducts = [];
+
    for(let button of addButtons){
       button.addEventListener('click', collectItems);
    }
 
-
-
+   checkOutButton.addEventListener('click', checkoutProcedure)
 
    function collectItems(e){
       let currentDiv = e.target.parentElement.parentElement;
@@ -22,11 +24,20 @@ function solve() {
 
       }
       else{
-         let currentObject = boughtProducts.filter(x => x.name == productName);
-         currentObject.price = currentObject.price + productPrice;
-         console.log(currentObject.price)
+         let currentObject = boughtProducts.find(x => x.name == productName);
+         currentObject.price += productPrice;
          
       }
+
+      targetOutput.value += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`
       
+   }
+
+   function checkoutProcedure(){
+      let totalBoughtProducts = [];
+      let totalPrice = 0;
+      boughtProducts.forEach(x => totalBoughtProducts.push(x.name));
+      boughtProducts.forEach(x => totalPrice += x.price);
+      targetOutput.value += `You bought ${totalBoughtProducts.join(", ")} for ${totalPrice.toFixed(2)}.`
    }
 }
