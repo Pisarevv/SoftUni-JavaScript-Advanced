b74f42bd9d4cffd6bd89a04d64b2bb90e6c72978
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-10-02 10:47:50 +0300

    Task 3 completed

---
+++ 6. Advanced Functions/CurrencyFormat.js
@@ -0,0 +1,39 @@
+function createFormatter(separator,symbol,symbolFirst,func){
+    let curSeparator = separator;
+    let curSymbol = symbol;
+    let curSymFirst = symbolFirst;
+    let formatFunction = func;
+
+    return (value) => {
+        return formatFunction(curSeparator,curSymbol,curSymFirst,value);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function currencyFormatter(separator, symbol, symbolFirst, value) {
+    let result = Math.trunc(value) + separator;
+    result += value.toFixed(2).substr(-2,2);
+    if (symbolFirst) return symbol + ' ' + result;
+    else return result + ' ' + symbol;
+}
+
+
+
+
+
+
+let dollarFormatter = createFormatter(',', '$', true, currencyFormatter);
+console.log
+console.log(dollarFormatter(5345));   // $ 5345,00
+console.log(dollarFormatter(3.1429)); // $ 3,14
+console.log(dollarFormatter(2.709));  // $ 2,71
