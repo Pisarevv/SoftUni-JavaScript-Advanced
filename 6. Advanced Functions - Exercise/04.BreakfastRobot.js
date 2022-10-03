045e33b7c03f6134e0d42dc17780dd9c5aaba19c
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-10-03 13:36:47 +0300

    Task 4 completed on 30%

---
+++ 6. Advanced Functions - Exercise/04.BreakfastRobot.js
@@ -0,0 +1,42 @@
+function robotCook(){
+
+    let recepies = createRecepie();
+    let ingredients  = createIngridients();
+
+
+
+    return {
+        restock : () => {
+
+        },
+        prepare : () => {
+
+        },
+        report : () => {
+
+        }
+    }
+
+
+
+
+    function createRecepie() {
+        return {
+            apple: {carbohydrate : 1, flavour : 2},
+            lemonade : {carbohydrate : 10 , flavour : 20},
+            burger : {carbohydrate : 10 ,fat : 7, flavour : 20},
+            eggs : {protein : 5 ,fat : 1, flavour : 1},
+            turkey : {protein : 10, carbohydrate : 10 ,fat : 10, flavour : 10},
+
+        }
+    }
+
+    function createIngridients(){
+        return {
+            carbohydrate : 0,
+            flavour : 0,
+            protein : 0,
+            fat : 0
+        }
+    }
+}
