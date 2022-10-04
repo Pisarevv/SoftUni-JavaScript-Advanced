b9fdf66ba389e0aa38767deab551ad4f547e9913
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-10-04 19:28:20 +0300

    Task 7 completed

--- 6. Advanced Functions - Exercise/07. Simple Calculator/app.js
+++ 6. Advanced Functions - Exercise/07. Simple Calculator/app.js
@@ -1,7 +1,31 @@
 function calculator() {
-    // TODO:
+
+    let selector1;
+    let selector2;
+    let resultSelector; 
+
+    return {
+        init : (selectedSelector1,selectedSelector2,selectedResultSelector) => {
+
+            selector1 = document.querySelector(selectedSelector1)
+            selector2 = document.querySelector(selectedSelector2);
+            resultSelector = document.querySelector(selectedResultSelector);         
+
+        },
+        add : () => {
+
+            resultSelector.value = Number(selector1.value) + Number(selector2.value);
+        },
+        subtract : () => {
+          
+            resultSelector.value = Number(selector1.value) - Number(selector2.value);
+        }
+    }
+
 }
 
 
+const calculate = calculator (); 
+calculate.init ('#num1', '#num2', '#result'); 
 
 
