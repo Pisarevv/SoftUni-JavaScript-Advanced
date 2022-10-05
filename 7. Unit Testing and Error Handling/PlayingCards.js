39d18beb86ba4a1cc7e275d7fd1fe307ae4dbb0b
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-10-05 20:42:47 +0300

    Task 2 not fully completed

--- 7. Unit Testing and Error Handling/PlayingCards.js
+++ 7. Unit Testing and Error Handling/PlayingCards.js
@@ -1,40 +1,48 @@
-const validFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-const validSuits = "SHDC";
+
 
 function cardObject (intpuFace,inputSuit) {
-    let card ='';
-    if(validFaces.indexOf(intpuFace) == -1){
-        console.log("Error");
-    }
-    if(!validSuits.includes(inputSuit)){
-        console.log("Error");
+    
+    const validFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const validSuits = "SHDC";
+    try {
+        if(validFaces.indexOf(intpuFace) == -1){
+            throw new error;
+        }
+        if(!validSuits.includes(inputSuit)){
+            throw new error;
+        }
     }
 
-    
+    catch (error) {
+
+      console.error(error);
+
+    }
+   
+
     if(inputSuit == 'S'){
-        console.log(`${intpuFace}${'\u2660'}`);
+        inputSuit = '\u2660';
     }
     if(inputSuit == 'H'){
-        onsole.log(`${intpuFace}${'\u2665'}`);
+        inputSuit = '\u2665';
     }
     if(inputSuit == 'D'){
-        onsole.log(`${intpuFace}${'\u2666'}`);
+        inputSuit = '\u2666';
     }
     if(inputSuit == 'C'){
-        onsole.log(`${intpuFace}${'\u2663'}`);
+        inputSuit = '\u2663';
     }
-
-    return card;
     
-     return function toString () {
 
-       
-
-
+    return {
+        face : intpuFace,
+        suit : inputSuit,
+        toString : () => {
+             console.log(`${intpuFace}${inputSuit}`);
+        }
     }
-
 
 }
 
-let card = cardObject('A', 'S');
+let card = cardObject('1', 'S');
 card.toString();
