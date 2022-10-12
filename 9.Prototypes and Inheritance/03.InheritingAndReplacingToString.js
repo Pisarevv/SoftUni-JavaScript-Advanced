2528dd1efc46387df32951d35bf46ab472b4c6be
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-10-12 10:05:49 +0300

    Task 3 completed

---
+++ 9.Prototypes and Inheritance/03.InheritingAndReplacingToString.js
@@ -0,0 +1,57 @@
+function toStringExtension() {
+
+    class Person{
+        constructor(name,email){
+            this.name = name;
+            this.email = email;
+        }
+
+        toString(){
+            return `Person (name: ${this.name}, email: ${this.email})`;
+        }
+    };
+
+    class Teacher extends Person{
+        constructor(name,email,subject){
+            super(name,email);
+            this.subject = subject;
+        }
+        toString(){
+            return `Teacher (name: ${this.name}, email: ${this.email}, subject: ${this.subject})`;
+            ('Teacher (name: Ivan, email: ivan@ivan.com, subject: Graphics)')
+        }
+    };
+
+
+    class Student extends Person{
+        constructor(name,email,course){
+            super(name,email);
+            this.course = course;
+        }
+        toString(){
+            return `Student (name: ${this.name}, email: ${this.email}, course: ${this.course})`;
+        }
+    };
+
+
+    return {
+        Person,
+        Teacher,
+        Student
+    }
+}
+
+
+let classes = toStringExtension();
+
+let Teacher = classes.Teacher;
+let Person = classes.Person;
+let Student = classes.Student;
+
+let p = new Person("Pesho",'Pesho@pesh.com');
+let t = new Teacher('Ivo','Ivo@pesho.com','JS')
+let s = new Student('Atanas','Atanas@asd.bg', "JS ADVANCEd")
+
+console.log(p.toString());
+console.log(t.toString());
+console.log(s.toString());
